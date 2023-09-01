1924f3c361aba0f9fa68f76e921788a7e8548490
Eric <abductee0@gmail.com>
2023-08-31 20:26:55 -0700

    Start to have some fun with the homepage

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,22 @@ const Home = () => {
 
       <main>
         <Header title='The Merovingian Architecture' />
-        <p className='description'>
+        <p>
+          <img style={{ width: '500px', maxWidth: '100%' }} src='https://merv-arch.s3.us-west-2.amazonaws.com/merovingian.jpg' />
+        </p>
+
+        <em>
+          "You see, there is only one constant, one universal, it is the only real truth: causality. Action. Reaction. Cause and effect." - the Merv
+        </em>
+
+        <br />
+        <hr width='800' />
+        <hr width='400' />
+        <hr width='200' />
+        <hr width='100' />
+        <hr width='50' />
+
+        <p>
           DDD, CQRS, Event Sourcing, Sanity
         </p>
       </main>
