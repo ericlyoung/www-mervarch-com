1b528d0bc3b716f1e850c1a73b361a8d67fa173d
Eric <ericlouisyoung@gmail.com>
2023-11-08 12:36:09 -0700

    Update

---
+++ pages/feasible-privacy-policy.js
@@ -0,0 +1,19 @@
+const FeasiblePrivacyPolicy = () => {
+  return (
+    <div style={{ padding: '1em' }}>
+      <h1 style={{ padding: '1em'}}>
+        Feasible App Privacy Policy
+      </h1>
+
+      <p style={{ padding: '0.2em 1em'}}>
+        Feasible gathers no data.
+      </p>
+
+      <p style={{ padding: '0.2em 1em'}}>
+        Feasible is used anonymously.
+      </p>
+    </div>
+  )
+}
+
+export default FeasiblePrivacyPolicy
